38a8018a6d987bffc81f6406c5f544b044a1e99d
Dongwoo Kim <dongwookim@Dongwooui-MacBookPro.local>
2022-07-04 21:01:01 +0900

    perf(dezero): made a package of dezero following step 23

---
+++ Dongwoo/dezero/core_simple.py
@@ -0,0 +1,225 @@
+import contextlib
+import numpy as np
+import weakref
+
+
+class Config:
+    enable_backprop=True
+
+class Variable:
+
+    __array_priority__=200
+    
+    def __init__(self, data, name=None): #added name=None
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError('{} is not supported'.format(type(data)))
+        
+        self.name=name #added
+        self.data = data
+        self.grad = None
+        self.creator = None
+        self.generation=0
+
+    def set_creator(self, func):
+        self.creator = func
+        self.generation= func.generation+1 
+
+    def backward(self, retain_grad=False):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = []
+        seen_set=set()
+        
+        def add_func(f):
+            if f not in seen_set:
+                funcs.append(f)
+                seen_set.add(f)
+                funcs.sort(key=lambda x: x.generation)
+        add_func(self.creator)
+        
+        while funcs:
+            f = funcs.pop()
+            gys = [output().grad for output in f.outputs] 
+            gxs = f.backward(*gys) 
+            if not isinstance(gxs, tuple): 
+                gxs=(gxs, )
+            for x, gx in zip(f.inputs, gxs): 
+                if x.grad is None:
+                    x.grad=gx
+                else:
+                    x.grad=x.grad+gx 
+            
+                if x.creator is not None:
+                    add_func(x.creator)
+            
+            if not retain_grad:
+                for y in f.outputs:
+                    y().grad=None
+                    
+    def cleargrad(self):
+        self.grad=None
+    
+    @property
+    def ndim(self):
+        return self.data.ndim
+    
+    @property
+    def shape(self):
+        return self.data.shape
+    
+    @property
+    def size(self):
+        return self.data.size
+    
+    @property
+    def dtype(self):
+        return self.data.dtype
+    
+    def __len__(self): 
+        return len(self.data)
+    
+    def __repr__(self): #print out the data inside Variable
+        if self.data is None:
+            return 'variable(None)'
+        p=str(self.data).replace('\n','\n'+''*9)
+        return 'variable('+p+')'
+
+class Function:
+    def __call__(self, *inputs):
+        inputs=[as_variable(x) for x in inputs] #added
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs)
+        if not isinstance(ys, tuple):
+            ys = (ys,)
+        outputs = [Variable(as_array(y)) for y in ys]
+        
+        if Config.enable_backprop:
+            self.generation=max([x.generation for x in inputs])         
+            for output in outputs:
+                output.set_creator(self)
+            self.inputs = inputs
+            self.outputs=[weakref.ref(output) for output in outputs] 
+        return outputs if len(outputs) > 1 else outputs[0]
+
+    def forward(self, xs):
+        raise NotImplementedError()
+
+    def backward(self, gys):
+        raise NotImplementedError()
+        
+class Add(Function):
+    def forward(self, x0, x1):
+        y=x0+x1
+        return y
+    def backward(self, gy):
+        return gy, gy
+
+class Mul(Function):
+    def forward(self, x0, x1):
+        y=x0*x1
+        return y
+    def backward(self, gy):
+        x0,x1=self.inputs[0].data, self.inputs[1].data
+        return gy*x1, gy*x0    
+    
+class Neg(Function):    
+    def forward(self, x):
+        return -x
+    def backward(self, gy):
+        return -gy
+
+class Sub(Function):
+    def forward(self, x0, x1):
+        y=x0-x1
+        return y
+    def backward(self, gy):
+        return gy, -gy
+    
+class Div(Function):
+    def forward(self, x0,x1):
+        y=x0/x1
+        return y
+    def backward(self, gy):
+        x0,x1=self.inputs[0].data, self.inputs[1].data
+        gx0=gy/x1
+        gx1=gy*(-x0/x1**2)
+        return gx0, gx1
+    
+class Pow(Function):
+    def __init__(self, c):
+        self.c=c
+    def forward(self, x):
+        y=x**self.c
+        return y
+    def backward(self, gy):
+        x=self.inputs[0].data
+        c=self.c
+        gx=c*x**(c-1)*gy
+        return gx
+    
+@contextlib.contextmanager #decorator to understand the context
+    
+def using_config(name, value):
+    old_value=getattr(Config, name)
+    setattr(Config, name, value)
+    try:
+        yield
+    finally:
+        setattr(Config, name, old_value)    
+
+def no_grad():
+    return using_config('enable_backprop', False)
+
+def as_variable(obj):
+    if isinstance(obj,Variable):
+        return obj
+    return Variable(obj)
+
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
+def add(x0, x1):
+    x1=as_array(x1)
+    return Add()(x0, x1) 
+
+def mul(x0,x1):
+    x1=as_array(x1)
+    return Mul()(x0,x1)
+
+def neg(x):
+    return Neg()(x)
+
+def sub(x0,x1):
+    x1=as_array(x1)
+    return Sub()(x0,x1)
+
+def rsub(x0,x1):
+    x1=as_array(x1)
+    return Sub()(x1,x0)
+
+def div(x0,x1):
+    x1=as_array(x1)
+    return Div()(x1,x0)
+
+def rdiv(x0,x1):
+    x1=as_array(x1)
+    return Div()(x1,x0)
+
+def pow(x,c):
+    return Pow(c)(x)
+
+def setup_variable():
+    Variable.__add__=add
+    Variable.__radd__=add
+    Variable.__mul__=mul
+    Variable.__rmul__=mul
+    Variable.__neg__=neg
+    Variable.__rsub__=rsub
+    Variable.__truediv__=div
+    Variable.__rtruediv__=rdiv
+    Variable.__pow__=pow
+    
