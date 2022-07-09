c1615880531e0d79092c49b47fe4111af5b7a554
Kangbeen Ko <sweetrainforyou@gmail.com>
2022-07-09 23:51:48 +0900

    test: test_intro

--- Kangbeen/steps/step04.py
+++ Kangbeen/steps/step04.py
@@ -1,11 +1,13 @@
 from step03 import *
 
+
 def numerical_diff(f, x, eps=1e-4):
-    x0 = Variable(x.data - eps) 
-    x1 = Variable(x.data + eps) 
-    y0 = f(x0) # f(x-h)
-    y1 = f(x1) # f(x+h)
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
+    y0 = f(x0)  # f(x-h)
+    y1 = f(x1)  # f(x+h)
     return (y1.data - y0.data) / (2*eps)
+
 
 def complex_f(x):
     A = Square()
@@ -19,7 +21,7 @@ if __name__ == '__main__':
     x = Variable(np.array(2.0))
     dy = numerical_diff(f, x)
     print(dy)
-    
+
     x = Variable(np.array(0.5))
     dy = numerical_diff(complex_f, x)
     print(dy)
