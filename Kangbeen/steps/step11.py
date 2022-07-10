9f14e011e5750dd7d39ef684c043de4e409eff64
Kangbeen Ko <sweetrainforyou@gmail.com>
2022-07-10 20:50:10 +0900

    feat(step11): 가변 길이 인수(순전파 편)

---
+++ Kangbeen/steps/step11.py
@@ -0,0 +1,69 @@
+import numpy as np
+
+
+class Variable:
+    def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError(f'{type(data)} type is not supported.')
+
+        self.data = data
+        self.grad = None
+        self.creator = None
+
+    def set_creator(self, func):
+        self.creator = func
+
+    def backward(self):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = [self.creator]
+        while funcs:
+            f = funcs.pop()
+            x, y = f.input, f.output
+            x.grad = f.backward(y.grad)
+
+            if x.creator is not None:
+                funcs.append(x.creator)
+
+
+def as_array(x):
+    if np.array(x):
+        return np.array(x)
+    return x
+
+
+class Function:
+    def __call__(self, inputs):
+        xs = [x.data for x in inputs]
+        ys = self.forward(xs)
+        outputs = [Variable(as_array(y)) for y in ys]
+
+        for output in outputs:
+            output.set_creator(self)
+
+        self.inputs = inputs
+        self.outputs = outputs
+        return outputs
+
+    def forward(self, xs):
+        raise NotImplementedError()
+
+    def backward(self, gys):
+        raise NotImplementedError()
+
+
+class Add(Function):
+    def forward(self, xs):
+        x0, x1 = xs
+        y = x0 + x1
+        return (y,)
+
+
+if __name__ == '__main__':
+    xs = [Variable(np.array(2)), Variable(np.array(3))]  # 리스트 형태로 input 준비
+    f = Add()
+    ys = f(xs)
+    y = ys[0]  # 튜플 첫번째 인수
+    print(y.data)
