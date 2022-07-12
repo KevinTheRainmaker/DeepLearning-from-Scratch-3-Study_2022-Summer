7e3f5095c07eff613c409fdb6e2f90f01e306763
Kangbeen Ko <sweetrainforyou@gmail.com>
2022-07-12 14:19:23 +0900

    feat(step14): 같은 변수 반복 사용

---
+++ Kangbeen/steps/step14.py
@@ -0,0 +1,105 @@
+import numpy as np
+
+
+class Variable:
+    def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError(f'{type(data)} type is not supported.')
+
+        self.data = data
+        self.grad = None
+        self.creator = None
+
+    def set_creator(self, func):
+        self.creator = func
+
+    def backward(self):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = [self.creator]
+        while funcs:
+            f = funcs.pop()
+            gys = [output.grad for output in f.outputs]
+            gxs = f.backward(*gys)
+            if not isinstance(gxs, tuple):
+                gxs = (gxs,)
+
+            for x, gx in zip(f.inputs, gxs):
+                if x.grad is None:
+                    x.grad = gx
+                else:
+                    x.grad = x.grad + gx
+
+                if x.creator is not None:
+                    funcs.append(x.creator)
+
+    def cleargrad(self):
+        self.grad = None
+
+
+def as_array(x):
+    if np.array(x):
+        return np.array(x)
+    return x
+
+
+class Function:
+    def __call__(self, *inputs):
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs)  # __call__ 에서 unpack
+        if not isinstance(ys, tuple):  # 튜플이 아닌 경우
+            ys = (ys,)
+        outputs = [Variable(as_array(y)) for y in ys]
+
+        for output in outputs:
+            output.set_creator(self)
+
+        self.inputs = inputs
+        self.outputs = outputs
+
+        return outputs if len(outputs) > 1 else outputs[0]
+
+
+class Add(Function):
+    def forward(self, x0, x1):
+        y = x0 + x1
+        return y
+
+    def backward(self, gy):
+        return gy, gy
+
+
+class Square(Function):
+    def forward(self, x):
+        y = x ** 2
+        return y
+
+    def backward(self, gy):
+        x = self.inputs[0].data  # 수정 전: x = self.input.data
+        gx = 2 * x * gy
+        return gx
+
+
+def add(x0, x1):
+    return Add()(x0, x1)
+
+
+def square(x):
+    f = Square()
+    return f(x)
+
+
+if __name__ == '__main__':
+    x = Variable(np.array(3.0))
+    y = add(x, x)
+    y.backward()
+    print(x.grad)
+
+    # 미분값 초기화
+    x.cleargrad()
+
+    y = add(add(x, x), x)
+    y.backward()
+    print(x.grad)
