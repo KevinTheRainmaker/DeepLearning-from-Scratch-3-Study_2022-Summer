564bf8cac1a3ab9f93fccf801e4f84846e350120
Kangbeen Ko <sweetrainforyou@gmail.com>
2022-07-21 17:36:55 +0900

    feat(step19): 변수 사용성 개선

---
+++ Kangbeen/steps/step19.py
@@ -0,0 +1,171 @@
+from memory_profiler import profile
+import numpy as np
+from queue import PriorityQueue
+import weakref
+import contextlib
+
+
+class Variable:
+    def __init__(self, data, name=None):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError(f'{type(data)} type is not supported.')
+
+        self.data = data
+        self.name = name
+        self.grad = None
+        self.creator = None
+        self.generation = 0
+
+    @property
+    def shape(self):
+        return self.data.shape
+
+    @property
+    def ndim(self):
+        return self.data.ndim
+
+    @property
+    def size(self):
+        return self.data.size
+
+    @property
+    def dtype(self):
+        return self.data.dtype
+
+    def __len__(self):
+        return len(self.data)
+
+    def __repr__(self):
+        if self.data is None:
+            return 'variable(None)'
+        p = str(self.data).replace('\n', '\n' + ' '*9)
+        return 'variable(' + p + ')'
+
+    def set_creator(self, func):
+        self.creator = func
+        self.generation = func.generation + 1
+
+    def backward(self, retain_grad=False):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = PriorityQueue()
+        seen_set = set()
+
+        def add_func(f):
+            if f not in seen_set:
+                funcs.put(f)
+                seen_set.add(f)
+
+        add_func(self.creator)
+
+        while not funcs.empty():
+            f = funcs.get()
+            gys = [output().grad for output in f.outputs]
+            gxs = f.backward(*gys)
+            if not isinstance(gxs, tuple):
+                gxs = (gxs,)
+
+            for x, gx in zip(f.inputs, gxs):
+                if x.grad is None:
+                    x.grad = gx
+                else:
+                    x.grad = x.grad + gx
+
+                if x.creator is not None:
+                    add_func(x.creator)
+
+            if not retain_grad:
+                for y in f.outputs:
+                    y().grad = None
+
+    def cleargrad(self):
+        self.grad = None
+
+
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
+
+class Config:
+    enable_backprop = True
+
+
+class Function:
+    def __call__(self, *inputs):
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs)
+        if not isinstance(ys, tuple):
+            ys = (ys,)
+        outputs = [Variable(as_array(y)) for y in ys]
+
+        if Config.enable_backprop:
+            self.generation = max([x.generation for x in inputs])
+
+            for output in outputs:
+                output.set_creator(self)
+
+            self.inputs = inputs
+            self.outputs = [weakref.ref(output) for output in outputs]
+
+        return outputs if len(outputs) > 1 else outputs[0]
+
+    def __lt__(self, other):
+        return self.generation > other.generation
+
+
+@contextlib.contextmanager
+def using_config(name, value):
+    old_value = getattr(Config, name)
+    setattr(Config, name, value)
+    try:
+        yield
+    finally:
+        setattr(Config, name, old_value)
+
+
+class Add(Function):
+    def forward(self, x0, x1):
+        y = x0 + x1
+        return y
+
+    def backward(self, gy):
+        return gy, gy
+
+
+class Square(Function):
+    def forward(self, x):
+        y = x ** 2
+        return y
+
+    def backward(self, gy):
+        x = self.inputs[0].data  # 수정 전: x = self.input.data
+        gx = 2 * x * gy
+        return gx
+
+
+def add(x0, x1):
+    return Add()(x0, x1)
+
+
+def square(x):
+    return Square()(x)
+
+
+def no_grad():
+    return using_config('enable_backprop', False)
+
+
+@profile
+# Memory Increment 확인
+def test():
+    with no_grad():
+        x = Variable(np.ones((100, 100, 100)))
+        y = square(square(square(x)))
+
+
+if __name__ == '__main__':
+    test()
